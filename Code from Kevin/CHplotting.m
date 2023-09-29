0a122d72a01390d8519bdce8e54425949450df27
Sarah Groves <sarahmaddoxgroves@gmail.com>
2023-09-29 13:05:16 -0400

    updated cpc movies without IC

--- Code from Kevin/CHplotting.m
+++ Code from Kevin/CHplotting.m
@@ -2,18 +2,19 @@ clear
 close all hidden
 warning off
 
-phi = readmatrix('/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/Code from Kevin/phi_CPC_circle.m','FileType','text');
+phi = readmatrix('/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/Code from Kevin/phi_CPC.m','FileType','text');
 phidims = size(phi);
-% phidims(3) = phidims(1)/phidims(2); %Determine number of frames captured
-% phidims(1) = phidims(2); %Determine size of square grid
-phidims(1) = 128;
-phidims(2) = 256;
-phidims(3) = 201;
+phidims(3) = phidims(1)/phidims(2); %Determine number of frames captured
+phidims(3) = phidims(1)/phidims(2); %Determine number of frames captured
+phidims(1) = phidims(2); %Determine size of square grid
+% phidims(1) = 128;
+% phidims(2) = 256;
+% phidims(3) = 201;
 phi = reshape(phi,phidims(1),phidims(3),phidims(2)); %Reshape multidimensional array
 phi = shiftdim(phi,2); %Shift dimensions to move frames to the third dimension
 myfig = figure();
-hold on
-phi_movie = VideoWriter('/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/C_output/phi_CPC_circle2.mp4','MPEG-4');
+% hold on
+phi_movie = VideoWriter('/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/C_output/phi_CPC.mp4','MPEG-4');
 phi_movie.Quality = 100; %Highest quality video
 open(phi_movie);
 for i = 1:phidims(3)
