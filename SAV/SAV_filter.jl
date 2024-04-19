067791c93b5e5423bbb35ea843417aa5b8905b4f
Sarah Groves <sarahmaddoxgroves@gmail.com>
2024-04-19 15:20:31 -0400

    converting SAV to julia

--- SAV/SAV_filter.jl
+++ SAV/SAV_filter.jl
@@ -1,23 +1,33 @@
 import Pkg
 # Pkg.add("FFTW")
 using FFTW
-
-
-# Pkg.add("PythonPlot")
 using Plots
+using LinearAlgebra
+# Pkg.add("PythonPlot")
 # using PythonPlot
 # pythonplot()
+# using Pkg;
+# Pkg.add(url="https://github.com/RobBlackwell/Parula.jl");
+# using Parula
 
+##############################
+######### Functions ##########
+##############################
+
+#MATLAB to JULIA
+# fft2 --> fft
+#real(fft2(x)) --> real.(fft(x))
+# ifft2 --> ifft
 
 function f(phi)
     a = 0.5
-    psi = (phi + 1) / 2
-    fphi = 4 * (psi .^ 2) .* (6 * a - 4 * (1 + a) * psi + 3 * psi .^ 2) / (3 * epsilon^2)
+    psi = (phi .+ 1) ./ 2
+    fphi = 4 .* psi .^ 2 .* (6 * a .- 4 * (1 + a) * psi .+ 3 * psi .^ 2) ./ (3 * epsilon^2)
     return fphi
 end
 
 function r0_fun(phi0)
-    r0 = @.sqrt(hx * hy * sum(sum(f(phi0))) .+ C0)
+    r0 = sqrt(hx * hy * sum(f(phi0)) + C0)
     return r0
 end
 
@@ -30,19 +40,45 @@ function Lap(phi)
     return Lphi
 end
 
-## Space parameters
-global Lx = 1
-global Ly = Lx
-para = Dict{String,Any}("Lx" => Lx)
-para["Ly"] = Ly
-Nx = 2^4
-para["Nx"] = Nx
-Ny = Nx
-para["Ny"] = Nx
-global hx = Lx / Nx
-global hy = Ly / Ny
-x = hx .* (0:Nx-1)
-y = hy .* (0:Ny-1)
+function A_inv_CN(phi)
+    Ai = real.(ifft(fft2_filtered(phi) ./ (1 .+ dt / 2 * M * k4 - dt / 2 * gamma0 / epsilon^2 * M * k2)))
+    return Ai
+end
+
+
+function df(phi)
+    #  dfphi = (phi.^3-(1+gamma0)*phi)/epsilon^2;
+    a = 0.5
+    dpsidphi = 1 / 2
+    psi = (phi .+ 1) ./ 2
+    dfphi = 16 * (1 .- psi) .* (a .- psi) .* psi / epsilon^2 * dpsidphi
+    return dfphi
+end
+
+function b_fun(phi)
+    E1 = fft2_filtered(f(phi))
+    b = df(phi) ./ sqrt(E1[1, 1] * hx * hy + C0)
+    return b
+end
+
+function g_fun_CN(phi0, r0, b)
+    bphi0 = fft2_filtered(b .* phi0)
+    bphi0 = hx * hy * bphi0[1, 1]
+    E1 = fft2_filtered(f(phi0))
+    g = phi0 - dt / 2 * M * Lap(Lap(phi0)) + dt / 2 * gamma0 / epsilon^2 * M * Lap(phi0) + dt * M .* Lap(b) * (r0 - 1 / 4 * bphi0 - 1 / 2 * Beta * dt * r0 * (r0 - sqrt(E1[1, 1] * hx * hy + C0)))
+    return g
+end
+
+function r_fun(phi, phi0, r0, b)
+    bphi0 = fft2_filtered(b .* phi0)
+    bphi0 = hx * hy * bphi0[1, 1]
+    bphi = fft2_filtered(b .* phi)
+    bphi = hx * hy * bphi[1, 1]
+
+    E1 = fft2_filtered(f(phi0))
+    r = r0 + 1 / 2 * (bphi - bphi0) - Beta * dt * r0 * (r0 - sqrt(E1[1, 1] * hx * hy + C0))
+    return r
+end
 
 
 function meshgrid(x, y)
@@ -50,14 +86,57 @@ function meshgrid(x, y)
     Y = [j for i in 1:length(x), j in y]
     return X, Y
 end
+##############################
+######## Parameters ##########
+##############################
 
+## Space parameters
+global Lx = 1
+global Ly = Lx
+Nx = 2^4
+Ny = Nx
+global hx = Lx / Nx
+global hy = Ly / Ny
+para = Dict{String,Any}("Lx" => Lx)
+para["Ly"] = Ly
+para["Nx"] = Nx
+para["Ny"] = Nx
+
+x = hx .* (0:Nx-1)
+y = hy .* (0:Ny-1)
 xx, yy = meshgrid(x, y)
 
 ## Sarah's parameters
 h = hx
 h2 = h^2
-gam = 8 * h / (2 * sqrt(2) * atanh(0.9)) # gam is our epsilon
+gam = 8 * h / (2 * sqrt(2) * atanh(0.9)) # gam = epsilon here
 max_it = 15000
+
+## Interface and energy parameters
+global epsilon = gam / 2^0
+global C0 = 0
+global M = epsilon^2
+para["epsilon"] = epsilon
+para["C0"] = C0
+para["M"] = M
+
+## Relaxation and stabilization
+global Beta = 0
+global gamma0 = 0
+para["Beta"] = Beta
+para["gamma0"] = gamma0
+
+## Time parameters
+global dt = 1e-1 * h2
+global dtout = 50
+global T = max_it * dt
+para["dt"] = dt
+para["dtout"] = dtout
+para["T"] = T
+
+##############################
+###### Initialize data #######
+##############################
 
 #Initial data by importing
 #phi0 = read from matrix
@@ -82,49 +161,20 @@ para["phi0"] = phi0
 
 
 ## Plot initial figure
-Plots.contour(x, y, real.(psi0),
+p = Plots.contour(x, y, real.(psi0),
     fill=true,
     xlabel="X",
     ylabel="Y",
     title="Initial Conditions",
-    size=(600, 550))
+    size=(600, 550),
+    palette=:viridis)
 
-# display(p)
+Plots.display(p)
 
-## Interface and energy parameters
-global epsilon = gam / 2^0
-para["epsilon"] = epsilon
-global C0 = 0
-para["C0"] = C0
-global M = epsilon^2
-para["M"] = M
+##############################
+#### Crank Nicolson scheme ###
+##############################
 
-## Relaxation and stabilization
-global Beta = 0
-para["Beta"] = Beta
-global gamma0 = 0
-para["gamma0"] = gamma0
-
-
-## Time parameters
-global dt = 1e-1 * h2
-para["dt"] = dt
-dtout = 50
-para["dtout"] = dtout
-T = max_it * dt
-para["T"] = T
-
-## CN Solver (phi collected every dtout results)
-# [phi, ~, E, ~, Phi, O] = CH2d_SAV_CN(para)
-# psi = (phi + 1) / 2
-# Psi = (Phi + 1) / 2
-# Plots.contour(x, y, real.(psi),
-#     fill=true,
-#     xlabel="X",
-#     ylabel="Y",
-#     title="psi")
-
-## Crank Nicolson scheme
 function CH2d_SAV_CN(para)
     # global dt epsilon k2 k4 C0 hx hy Lx Ly gamma0 Beta M
     T = para["T"]
@@ -142,8 +192,11 @@ function CH2d_SAV_CN(para)
     phi0 = para["phi0"]
 
     t = 0
-    Nt = round(T / dt)
+    Nt = Int.(T / dt)
     E = zeros(Int.(Nt + 1))
+    D = zeros(Int.(Nt + 1))
+    O = zeros(Int.(Nt + 1))
+
 
     x = hx * (0:Nx-1)
     y = hy * (0:Ny-1)
@@ -157,16 +210,80 @@ function CH2d_SAV_CN(para)
     # % k2 = k_xx+k_yy
     global k4 = k2 .^ 2
 
-    # % phi  = phi0
-    # % r0 = para.r0
     r0 = r0_fun(phi0)
-    # % r  = r0
     Phi = phi0
     # on each iteration, Phi = vcat(Phi, phi)
 
     #initial modified energy
     Lphi = Lap(phi0)
-    E[1] = hx * hy * sum(sum(1 / 2 * phi0 .* (-Lphi))) .+ r0^2
+    E[1] = hx * hy * sum(1 / 2 .* phi0 .* (-Lphi)) + r0^2
+
+    #Error
+    D[1] = (r0 - sqrt(hx * hy * sum(f(phi0)))) / sqrt(hx * hy * sum(f(phi0)))
+    O[1] = norm(imag.(fft(phi0)))
+
+    # Time iteration
+    for nt in 1:Nt
+        t = t + dt
+        phi_bar = A_inv_CN(phi0 + dt / 2 * M * Lap(df(phi0)))
+
+        # Step 1
+        b = b_fun(phi_bar)
+
+        g = g_fun_CN(phi0, r0, b)
+
+        AiLb = A_inv_CN(M * Lap(b))
+        Aig = A_inv_CN(g)
+
+        gamma = -fft2_filtered(b .* AiLb)
+        gamma = gamma[1, 1] * hx * hy
+
+        # Step 2
+        bphi = fft2_filtered(b .* Aig)
+        bphi = bphi[1, 1] * hx * hy / (1 + dt / 4 * gamma)
+
+        # Step 3
+        phi = dt / 4 * bphi .* AiLb + Aig
+        r = r_fun(phi, phi0, r0, b)
+
+        # Update phi0, phi1, r1
+        phi0 = phi
+        r0 = r
+
+        # print out
+        if mod(nt, dtout) == 0
+            ll = Int.(nt / dtout)
+            tt = Nx*ll+1:Nx*(ll+1)
+            Phi = cat(Phi, phi, dims=1)
+        end
+
+
+        #Modified energy
+        E[1+nt] = hx * hy * sum(1 / 2 .* phi .* (-Lap(phi))) + r0^2
+
+
+        # Error
+        D[1+nt] = (r0 - sqrt(hx * hy * sum(f(phi)))) / sqrt(hx * hy * sum(f(phi)))
+        O[1+nt] = norm(imag.(fft(phi)))
+    end
+    return phi, r, E, D, Phi, O
 end
 
-CH2d_SAV_CN(para)
+
+##############################
+######## Run Solver ##########
+##############################
+
+# CN Solver (Phi collected every dtout results)
+# [phi, ~, E, ~, Phi, O] = 
+phi, r, E, D, Phi, O = CH2d_SAV_CN(para)
+psi = (phi .+ 1) ./ 2
+Psi = (Phi .+ 1) ./ 2
+
+Plots.contour(x, y, real.(psi),
+    fill=true,
+    xlabel="X",
+    ylabel="Y",
+    title="Psi at time $T",
+    size=(600, 550),
+    palette=:viridis)
