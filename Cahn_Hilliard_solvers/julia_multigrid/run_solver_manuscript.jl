4b0f2eef8bc79abce7456d2f7ae829a4f6de255e
Sarah Groves <sarahmaddoxgroves@gmail.com>
2024-06-12 14:28:40 -0400

    droplet r0 = 0.2

--- Cahn_Hilliard_solvers/julia_multigrid/run_solver_manuscript.jl
+++ Cahn_Hilliard_solvers/julia_multigrid/run_solver_manuscript.jl
@@ -7,13 +7,13 @@
 # Pkg.add("DelimitedFiles")
 # Pkg.add("LinearAlgebra")
 # Pkg.add("Printf")
+using Dates
 include("./CH_multigrid_solver.jl")
 
-
+#%%
 #########################
 # Single droplet example
 #########################
-#%%
 nx = 128
 tol = 1e-6
 dt_0 = 2.5e-5
@@ -21,12 +21,17 @@ M = 8
 gam = M * (1 / 128) / (2 * sqrt(2) * atanh(0.9))
 dts = [0.25 * dt_0, 0.5 * dt_0, dt_0, 2 * dt_0, 4 * dt_0]
 total_time = 1
-phi = initialization(nx, nx, method="droplet", h=1 / 128, gam=gam)
-outdir = "/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/Cahn_Hilliard_solvers/julia_multigrid/manuscript_output/droplet_2"
+phi = initialization(nx, nx, method="droplet", h=1 / 128, gam=gam, R0=0.2)
+outdir = "/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/Cahn_Hilliard_solvers/julia_multigrid/manuscript_output/droplet_R0_0.2_v2"
 for dt in dts
     println(dt)
     max_it = Int.(total_time / dt)
-    main(phi, nx, tol, outdir, dt=dt, gam=gam, max_it=max_it, print_mass=true, print_e=true, overwrite=false, print_r=true, suffix="_dt_$dt", check_dir=false)
+    date_time = now()
+    max_it_CH = 10000
+    time_passed = @elapsed main(phi, nx, tol, outdir, dt=dt, gam=gam, max_it=max_it, print_mass=false, print_e=false, overwrite=false, print_r=false, suffix="_dt_$dt", check_dir=false)
+    open("/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/Cahn_Hilliard_solvers/Job_specs.csv", "a", lock=false) do f
+        writedlm(f, [date_time "droplet_R0_0.2" "Julia" nx gam dt tol max_it max_it_CH time_passed], ",")
+    end
 end
 
 #%%
@@ -39,13 +44,17 @@ dt_0 = 2.5e-5
 M = 8
 gam = M * (1 / 128) / (2 * sqrt(2) * atanh(0.9))
 dts = [0.25 * dt_0, 0.5 * dt_0, dt_0, 2 * dt_0, 4 * dt_0]
-outdir = "/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/Cahn_Hilliard_solvers/julia_multigrid/manuscript_output/spinodal_2"
+outdir = "/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/Cahn_Hilliard_solvers/julia_multigrid/manuscript_output/spinodal_same_total_time"
 total_time = 0.02
 phi = initialization_from_file("$outdir/phi_128_initial.txt", nx, nx, delim=' ')
 for dt in dts
     println(dt)
     max_it = Int.(total_time / dt)
-    main(phi, nx, tol, outdir, dt=dt, gam=gam, max_it=max_it, print_mass=true, print_e=true, overwrite=false, print_r=true, suffix="dt_$dt", check_dir=false)
+    date_time = now()
+    time_passed = @elapsed main(phi, nx, tol, outdir, dt=dt, gam=gam, max_it=max_it, print_mass=true, print_e=true, overwrite=false, print_r=true, suffix="dt_$dt", check_dir=false)
+    open("/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/Cahn_Hilliard_solvers/Job_specs.csv", "a", lock=false) do f
+        writedlm(f, [date_time "droplet_R0_0.2" "Julia" nx gam dt tol max_it max_it_CH time_passed], ",")
+    end
 end
 
 #%%
--- Cahn_Hilliard_solvers/plotting/level_set_radius.m
+++ Cahn_Hilliard_solvers/plotting/level_set_radius.m
@@ -1,52 +1,71 @@
-dt_name = "6.25e-6";
-indir="/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/Cahn_Hilliard_solvers/julia_multigrid/manuscript_output/droplet";
-total_time=0.0625;
-dt = str2double(dt_name);
-timesteps=total_time/dt;
-name=sprintf('phi_128_%s_1.0e-6__dt_%s',string(timesteps), dt_name);
-dtout=10;
-Nx = 2^7;Ny=2^7;
-hx = 1/Nx;
-hy = 1/Ny;
-x  = hx*(0:Nx-1);           
-y  = hy*(0:Ny-1);
-[xx,yy] = meshgrid(x,y); 
-T0=0;
-tt=0;rr=0;everyR=10;
-phi = readmatrix(sprintf('%s/%s.txt', indir, name),'FileType','text');
-% phi = readmatrix("/Users/smgroves/Documents/GitHub/jlCHSolver/output.txt");
-phidims = size(phi);
-phidims(3) = phidims(1)/phidims(2); %Determine number of frames captured
-phidims(1) = phidims(2); %Determine size of square grid
-phi = reshape(phi,phidims(1),phidims(3),phidims(2)); %Reshape multidimensional array
-phi = shiftdim(phi,2); %Shift dimensions to move frames to the third dimension
+indir="/Users/smgroves/Documents/GitHub/Cahn_Hilliard_Model/Cahn_Hilliard_solvers/julia_multigrid/manuscript_output/droplet_R0_0.2";
+total_time=1;
+everyR=10;
+rrs = [];tts=[];dts=[];
+for dt_name = ["0.0001" "5.0e-5" "2.5e-5" "1.25e-5" "6.25e-6"]
+    [rr,tt] = level_set_plot(dt_name, indir, total_time, everyR);
+    dt = str2double(dt_name);
+    dt_vector = repmat(dt, 1,length(rr));
 
-for i = 0:everyR:timesteps/dtout
-    T=i*dtout*dt;
-    % t = Nx*i+1 : Nx*(i+1);
-    % psi = Psi(t,:);
-    phi_tmp = phi(:,:,i+1);
-    % Find and plot the 0.5 contour
-    figure('visible', 'off');
-    [~, h] = contour(x, y, phi_tmp, [0 0]);
-    % Extract the x and y coordinates of the 0 contour
-    contour_data = h.ContourMatrix;
-    x_contour = contour_data(1, 2:end); 
-    y_contour = contour_data(2, 2:end);
-    distances = sqrt((x_contour-0.5).^2 + (y_contour-0.5).^2);
-    radius = mean(distances);
-    close
-    tt(i/everyR+1) = T0 + T;
-    rr(i/everyR+1) = radius;
+    rrs = cat(2,rrs,rr);
+    tts = cat(2,tts,tt);
+    dts = cat(2,dts,dt_vector);
 end
-% clear h; 
 
-%% Plot the figure
-    fig = figure;
-        plot(tt,rr, ...
-                                    ':', ...
-            'LineWidth',1.5);set(gca,'FontSize',16);
-        title(sprintf('Radius at 0.5 level set %s',name))
-        xlabel('t');ylabel('R');
-        filename = sprintf('%s/radius_0.5_nx_128_nt_%s_1.0e-6__dt_%s.png',indir,string(timesteps),dt_name);
-        saveas(fig, filename);
+% data = transpose(cat(1, rrs,tts,dts));
+% Create a table with the data and variable names
+T = table(transpose(rrs), transpose(tts), transpose(dts),'VariableNames',{'radius', 'time', 'dt'});
+% Write data to text file
+writetable(T,sprintf('%s/radius_0.5_level_set.txt',indir))
+
+function [rr, tt] = level_set_plot(dt_name, indir, total_time, everyR)
+    dt = str2double(dt_name);
+    timesteps=total_time/dt;
+    name=sprintf('phi_128_%s_1.0e-6__dt_%s',string(timesteps), dt_name)
+    dtout=10;
+    Nx = 2^7;Ny=2^7;
+    hx = 1/Nx;
+    hy = 1/Ny;
+    x  = hx*(0:Nx-1);           
+    y  = hy*(0:Ny-1);
+    [xx,yy] = meshgrid(x,y); 
+    T0=0;
+    tt=0;rr=0;
+    phi = readmatrix(sprintf('%s/%s.txt', indir, name),'FileType','text');
+    % phi = readmatrix("/Users/smgroves/Documents/GitHub/jlCHSolver/output.txt");
+    phidims = size(phi);
+    phidims(3) = phidims(1)/phidims(2); %Determine number of frames captured
+    phidims(1) = phidims(2); %Determine size of square grid
+    phi = reshape(phi,phidims(1),phidims(3),phidims(2)); %Reshape multidimensional array
+    phi = shiftdim(phi,2); %Shift dimensions to move frames to the third dimension
+
+    for i = 0:everyR:timesteps/dtout
+        T=i*dtout*dt;
+        % t = Nx*i+1 : Nx*(i+1);
+        % psi = Psi(t,:);
+        phi_tmp = phi(:,:,i+1);
+        % Find and plot the 0.5 contour
+        figure('visible', 'off');
+        [~, h] = contour(x, y, phi_tmp, [0 0]);
+        % Extract the x and y coordinates of the 0 contour
+        contour_data = h.ContourMatrix;
+        x_contour = contour_data(1, 2:end); 
+        y_contour = contour_data(2, 2:end);
+        distances = sqrt((x_contour-0.5).^2 + (y_contour-0.5).^2);
+        radius = mean(distances);
+        close
+        tt(i/everyR+1) = T0 + T;
+        rr(i/everyR+1) = radius;
+    end
+    % clear h; 
+
+    %% Plot the figure
+        fig = figure('visible', 'off');
+            plot(tt,rr, ...
+                                        ':', ...
+                'LineWidth',1.5);set(gca,'FontSize',16);
+            title(sprintf('Radius at 0.5 level set %s',name))
+            xlabel('t');ylabel('R');
+            filename = sprintf('%s/radius_0.5_nx_128_nt_%s_1.0e-6__dt_%s.png',indir,string(timesteps),dt_name);
+            saveas(fig, filename);
+end
